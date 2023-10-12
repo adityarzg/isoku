f8ea0f9c08cc7b3f795353b991b9acdf1e1ef2e1
adityarzg <adityarzg@gmail.com>
2023-10-13 01:38:31 +0700

    setup hosting database

--- application/config/database.php
+++ application/config/database.php
@@ -94,3 +94,25 @@ $db['default'] = array(
 	'failover' => array(),
 	'save_queries' => TRUE
 );
+
+$db['hosting'] = array(
+	'dsn'	=> '',
+	'hostname' => 'localhost',
+	'username' => 'ison6688_isoku_user',
+	'password' => 'isokushop2023',
+	'database' => 'ison6688_isoku',
+	'dbdriver' => 'mysqli',
+	'dbprefix' => '',
+	'pconnect' => FALSE,
+	'db_debug' => (ENVIRONMENT !== 'production'),
+	'cache_on' => FALSE,
+	'cachedir' => '',
+	'char_set' => 'utf8',
+	'dbcollat' => 'utf8_general_ci',
+	'swap_pre' => '',
+	'encrypt' => FALSE,
+	'compress' => FALSE,
+	'stricton' => FALSE,
+	'failover' => array(),
+	'save_queries' => TRUE
+);
